a7ad18c20ea0b1f9fce686910fff6405e6ddfd4a
dhaval <dhaval.pinara@gmail.com>
2021-06-27 20:55:52 +0530

    Added CKAD notes

--- cka/foundation.sh
+++ cka/foundation.sh
@@ -1,5 +1,16 @@
 
 ########################################################
+#   Exam
+########################################################
+
+alias k=kubectl
+alias c=clear
+alias kgns="kubectl config  view| grep namespace"
+function ksns() { kubectl config set-context $(kubectl config current-context) --namespace=$1; }
+export force="--force --grace-period=0"
+export dd="--dry-run=client -o yaml"
+
+########################################################
 #   Foundational and useful command
 ########################################################
 2  echo "source <(kubectl completion bash)" >> ~/.bashrc
@@ -19,3 +30,51 @@
 
 29  kubectl cluster-info
 30  kubectl cluster-info dump
+
+
+########################################################
+#   Validation
+########################################################
+
+kubectl explain Pod.spec.serviceAccount --recursive | grep "containers\|serviceAccount"
+
+set list
+set listchars=tab:>-
+
+root@controlplane:~# cat /root/curl-test.sh
+for i in {1..35}; do
+   kubectl exec --namespace=kube-public curl -- sh -c 'test=`wget -qO- -T 2  http://webapp-service.default.svc.cluster.local:8080/info 2>&1` && echo "$test OK" || echo "Failed"';
+   echo ""
+done
+
+root@controlplane:~# k exec -it  curl -- sh
+/ # wget -qO- -T 2  http://webapp-service.default.svc.cluster.local:8080/info
+Hello, Application Version: v1 ; Color: blue/ #
+/ # wget --help
+BusyBox v1.30.1 (2021-03-31 04:59:34 UTC) multi-call binary.
+
+Usage: wget [-c|--continue] [--spider] [-q|--quiet] [-O|--output-document FILE]
+        [--header 'header: value'] [-Y|--proxy on/off] [-P DIR]
+        [-S|--server-response] [-U|--user-agent AGENT] [-T SEC] URL...
+
+Retrieve files via HTTP or FTP
+
+        --spider        Only check URL existence: $? is 0 if exists
+        -c              Continue retrieval of aborted transfer
+        -q              Quiet
+        -P DIR          Save to DIR (default .)
+        -S              Show server response
+        -T SEC          Network read timeout is SEC seconds
+        -O FILE         Save to FILE ('-' for stdout)
+        -U STR          Use STR for User-Agent header
+        -Y on/off       Use proxy
+
+
+
+
+    command:
+      - "sleep"
+      - "5000"
+
+
+    command: [ "/bin/sh", "-c", "sleep" , "5000" ]
